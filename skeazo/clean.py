bc5d56cd82e7ba9954b9e4ebb6a6b3b1b4e89f6c
Antoine <antoine@2ohm.fr>
2024-12-13 16:36:17 +0100

    Set non breakable space before ":.?!"

--- skeazo/clean.py
+++ skeazo/clean.py
@@ -3,7 +3,11 @@
 
 import logging
 
+import re
+import os
 import helpers.documents
+
+CHAR_NBSP = ' '
 
 logger = logging.getLogger(__name__)
 
@@ -14,10 +18,37 @@ def clean(args):
 
     logging.debug("Clean document {}".format(document_id))
 
-    with open(document_raw_md_path, 'r', encoding='utf-8') as file_in:
-        document = file_in.read()
-
-    # Currently, do nothing
+    if os.path.isfile(document_cleaned_md_path):
+        # If a cleaned file already exist, cleaning rules are applied on it
+        with open(document_cleaned_md_path, 'r', encoding='utf-8') as file_in:
+            document = file_in.readlines()
+    elif os.path.isfile(document_raw_md_path):
+        with open(document_raw_md_path, 'r', encoding='utf-8') as file_in:
+            document = file_in.readlines()
+    else:
+        logging.error("Mardown file not found.")
+        logging.error(f"Run ./skeazo-cli.py convert {document_id}")
+        return
 
     with open(document_cleaned_md_path, 'w', encoding='utf-8') as file_out:
-        file_out.write(document)
+        in_header = False
+
+        for line in document:
+            # Header is not cleaned
+            if line == "---\n":
+                in_header = not in_header
+                file_out.write(line)
+            elif in_header:
+                file_out.write(line)
+            else:
+                file_out.write(do_cleaning(line))
+
+def do_cleaning(line):
+    # Strip leading and trailling spaces
+    line = line[:-1].strip()
+
+    # Set non breakable space before : ":;?!"
+    pattern_punctution = re.compile(r"\b\s?(?P<punctuation>[:;?!])")
+    line = pattern_punctution.sub(f"{CHAR_NBSP}\g<punctuation>", line)
+
+    return f"{line}\n"
